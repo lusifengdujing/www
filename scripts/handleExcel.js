90414b5f64765b7595d2bfb007836285dbb9c413
lusifeng <lsf493362328@qq.com>
2019-03-26 02:07:36 +0800

    handleExcel writeFile.js 代码修改 writeFile.js 生成相应设备名文件夹还未完成

--- scripts/handleExcel.js
+++ scripts/handleExcel.js
@@ -1,7 +1,7 @@
 //解析上传的excel文件并生成相应的文件
 let XLSX=require('xlsx');
-let fs=require('fs');
-let path = require('path');
+// let fs=require('fs');
+// let path = require('path');
 let writeFile=require('./writeFile.js');
 
 
@@ -10,50 +10,21 @@ function handleExcel(req,res,next){
 	let fileName=req.file.originalname;
 	let workbook=XLSX.readFile("./upload/"+fileName,{});	
 	
-	// 将excel的工作笔Index和Var的内容加入到excelWorkbook对象中
-	let excelWorkbook={
-		sheetIndex:XLSX.utils.sheet_to_json(workbook.Sheets['Index']),
-		sheetVar:XLSX.utils.sheet_to_json(workbook.Sheets['Var'])
-	}
+	//sheetIndex:Index工作表
+	//sheetVar:Var工作表
+	//devices:Index表中的设备数组
+	let sheetIndex = XLSX.utils.sheet_to_json(workbook.Sheets['Index']);
+    let sheetVar = XLSX.utils.sheet_to_json(workbook.Sheets['Var']);
+	let devices=[];
+	// 获取设备号
+    for (let i = 0, length = sheetIndex.length; i < length; i++) {
+        devices[i] = sheetIndex[i]["包含设备号"];
+	}	
 
-
-
-	//生成Driver文件
-	writeFile.writeDriver(excelWorkbook);
-
-
-	
-	let XMLWriter = require('xml-writer');
-	//Drive文件生成
-
-	let	driveFile = new XMLWriter(true);
-	
-	
-    driveFile.startDocument('1.0', 'UTF-8');	
-	
-	driveFile.startElement('root');
-	driveFile.startElement('points');
-	// for(let i=0,length=jsonVar.length;i<length;i++){
-	// 	// 编写节点
-	// 	driveFile.startElement('Item')
-	// 	.writeAttribute('Name', jsonVar[i]["名称"])
-	// 	.writeAttribute('Address', jsonVar[i]["起始地址"])
-	// 	.endElement();  		
-	// }	
-	driveFile.endDocument(); 
-	console.log(driveFile.toString());
-	
-	// for(let i=0,length=jsonIndex.length;i<length;i++){
-	// 	let file = path.join('/Project/Driver/OPC', jsonIndex[i]["包含设备号"]+'.device'); 
-	// 	fs.writeFile(file,driveFile.toString(),(err)=> {
-	// 	if (err) {
-	// 	return console.log(err);
-	// 	}
-	// 	console.log('文件创建成功，地址：' + file);
-	// 	});
-	// }
-	
-	
+	//生成Driver文件	
+	writeFile.writeDriver(sheetIndex,sheetVar,devices);
+	//生成Rtdb文件
+	writeFile.writeRtdb(sheetIndex,sheetVar,devices);
 }
 
 
--- scripts/writeFile.js
+++ scripts/writeFile.js
@@ -1,55 +1,81 @@
 let XMLWriter = require('xml-writer');
 let path = require('path');
-let writeFile=require('./writeFile.js');
-let fs=require('fs');
+let writeFile = require('./writeFile.js');
+let fs = require('fs');
 
-let writeDriver = function (excelWorkbook) {
-    // 用来存放生成的文件
-    // 用来存放设备号
+
+// 生成Driver文件
+let writeDriver = function (sheetIndex, sheetVar, devices) {
+    // 用来存放生成的文件    
     let files = [];
-    let devices = [];
-
-    let sheetIndex = excelWorkbook["sheetIndex"];
-    let sheetVar = excelWorkbook["sheetVar"];
-
-
-    // 获取设备号
-    for (let i = 0, length = sheetIndex.length; i < length; i++) {
-        devices[i] = sheetIndex[i]["包含设备号"];
-    }
-    console.log(devices);
-
 
     for (let i = 0, length = devices.length; i < length; i++) {
         let driveFile = new XMLWriter(true);
-        driveFile.startDocument('1.0', 'UTF-8').startElement('root').startElement('points');
+        driveFile.startDocument('1.0', 'UTF-8')
+            .startElement('root')
+            .writeAttribute('Name', devices[i])
+            .writeAttribute('RemoteAddress', sheetIndex[i]["PLC IP地址"])
+            .writeAttribute('RemotePort', sheetIndex[i]["PLC 通讯端口号"])
+            .startElement('points');
         for (let j = 0, length = sheetVar.length; j < length; j++) {
+            // 判断是否读取的空格数据
+            if(sheetVar[j]["名称"]===" ") break;
             driveFile.startElement('Item')
-                .writeAttribute(devices[i] + '/Name', sheetVar[j]["名称"]);
-                let a=sheetVar[j]["起始地址"];                
-                driveFile.writeAttribute('Address', sheetVar[j]["起始地址"].replace(/[%]/g,""))
-                    .endElement();
-            
+                .writeAttribute('Name', devices[i] + '/' + sheetVar[j]["名称"])
+                .writeAttribute('Desc', sheetVar[j]["描述"])
+                .writeAttribute('Address', sheetVar[j]["起始地址"].replace(/[%]/, ""))
+                .endElement();
         }
-        files[i]=driveFile.toString();
-        console.log(files[i]);
+        files[i] = driveFile.toString();
+        // console.log(files[i]);
 
         // 写入文件
-		let file = path.join('/Project/Driver/OPC',devices[i]+'.device'); 
-		fs.writeFile(file,driveFile.toString(),(err)=> {
-		if (err) {
-		return console.log(err);
-		}
-		console.log('文件创建成功，地址：' + file);
-		});
-	}
+        let file = path.join('/Project/Driver/OPC', devices[i] + '.device');
+        fs.writeFile(file, driveFile.toString(), (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('文件创建成功，地址：' + file);
+        });
+    }
+}
+
+// 生成Rtdb文件
+let writeRtdb = function (sheetIndex, sheetVar, devices) {
+    let files = [];
+
+    for (let i = 0, length = devices.length; i < length; i++) {
+        let rtdbFile = new XMLWriter(true);
+        rtdbFile.startDocument('1.0', 'UTF-8')
+            .startElement('Item');            
+        for (let j = 0, length = sheetVar.length; j < length; j++) {
+            // 判断是否读取的空格数据
+            if(sheetVar[j]["名称"]===" ") break;
+            rtdbFile.startElement('Item')
+                .writeAttribute('Name', sheetVar[j]["名称"])
+                .writeAttribute('Desc', sheetVar[j]["描述"])
+            if(sheetVar[j]["数据类型"]==="Boolean"){
+                rtdbFile.writeAttribute('Type','Boolean');
+            }                
+            rtdbFile.endElement();
+        }
+        files[i] = rtdbFile.toString();
+        console.log(files[i]);
+
+        // 写入文件,在RealTimeDatabase文件下生成相应的文件并保存Variables.var文件
+        // let file = path.join('/Project/Driver/OPC', devices[i] + '.device');
+        // fs.writeFile(file, driveFile.toString(), (err) => {
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     console.log('文件创建成功，地址：' + file);
+        // });
     }
 
-
-
-
+}
 
 
 module.exports = {
-    writeDriver: writeDriver
+    writeDriver: writeDriver,
+    writeRtdb:writeRtdb
 }
